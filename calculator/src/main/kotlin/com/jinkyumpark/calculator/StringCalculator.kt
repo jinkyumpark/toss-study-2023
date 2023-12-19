387b8e3ec5acfc766c566a3876bb32b4f8b32e92
Jinkyum Park <jinpark1025@gmail.com>
2023-12-20 01:39:31 +0900

    [P1 - Calculator] implement StringCalculator.add without looking further in the book

--- calculator/src/main/kotlin/com/jinkyumpark/calculator/StringCalculator.kt
+++ calculator/src/main/kotlin/com/jinkyumpark/calculator/StringCalculator.kt
@@ -2,6 +2,29 @@ package com.jinkyumpark.calculator
 
 class StringCalculator {
     fun add(text: String?): Int {
-        TODO()
+        if (text.isNullOrEmpty()) {
+            return 0
+        }
+
+        if (text.isInt()) {
+            return text.toInt()
+        }
+
+        val separated = text.split(Regex(":|,|//.*\n"))
+        val numbers = separated.first().toInt() to separated.last().toInt()
+
+        if (numbers.first.isNegative() || numbers.second.isNegative()) {
+            throw RuntimeException("Numbers cannot be negative. ${numbers.first}, ${numbers.second}")
+        }
+
+        return numbers.first + numbers.second
     }
+}
+
+private fun Int.isNegative(): Boolean {
+    return this < 0
+}
+
+private fun String.isInt(): Boolean {
+    return this.all { it.isDigit() }
 }
